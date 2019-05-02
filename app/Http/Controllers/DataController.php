c5dab8b8ead039d6ec19270084081bb4bf2ef117
Patrick <pcallahan3@mail.greenriver.edu>
2019-05-02 09:02:18 -0700

    Added first route

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Data;
 
 class DataController extends Controller
 {
@@ -13,7 +14,8 @@ class DataController extends Controller
      */
     public function index()
     {
-        //
+        $data = Data::all();
+        return response()->json($data);
     }
 
     /**
